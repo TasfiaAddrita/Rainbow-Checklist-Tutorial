53ede156a4ead35c9c4fd9f6332f4abbc351d282
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-09-03 00:51:51 -0700

    completed all stretch, need to complete tests

--- rainbow_checklist.py
+++ rainbow_checklist.py
@@ -1,4 +1,6 @@
 from termcolor import colored
+from os import system, name
+from time import sleep
 
 checklist = list()
 
@@ -7,14 +9,12 @@ def create(item):
     print("You added {} to the list.".format(color_text(item)))
 
 def read(index):
-    return "reading index {}: {}".format(index, color_text(checklist[int(index)]))
+    return "Reading index {}: {}".format(index, color_text(checklist[int(index)]))
 
 def update(index, new_item):
     prev_item = checklist[index]
     checklist[index] = new_item
     print("You updated {} to {}".format(color_text(prev_item), color_text(new_item)))
-    # print("You updated {} to {}".format(prev_item, new_item))
-
 
 def destroy(index):
     del_item = checklist[index]
@@ -25,14 +25,21 @@ def list_all_items():
     print("Printing all items: ")
     index = 0
     for item in checklist:
-        print("{} {}".format(index, color_text(item)))
+        print("Index {} - {}".format(index, color_text(item)))
         index += 1
 
-def mark_completed(index):
+def mark_complete(index):
+    print("You marked {} as complete.".format(color_text(checklist[index])))
     checklist[index] = "√ {}".format(checklist[index])
 
 def mark_uncomplete(index):
-    checklist[index] = "{}".format(checklist[index])
+    checklist[index] = "{}".format(checklist[index].replace("√ ", ""))
+    print("You marked {} as uncomplete.".format(color_text(checklist[index])))
+
+def check_complete(index):
+    if "√" in checklist[index]:
+        return True
+    return False
 
 def user_input(prompt):
     user_input = input(prompt)
@@ -55,12 +62,23 @@ def color_text(item):
             item_list[index] = colored(item_list[index], item_list[index])
     return " ".join(item_list)
 
+# CREDITS GO TO GEEKS FOR GEEKS: https://www.geeksforgeeks.org/clear-screen-python/
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
 def select(function_code):
+    print("-----------------------------------")
+
     if (function_code != "C" and function_code != "Q") and (len(checklist) == 0):
         print("Please pick an option from the list.")
 
     elif function_code == "C":
-        input_item = user_input("Input item: ")
+        input_item = user_input("Add item: ")
         create(input_item)
 
     elif function_code == "R":
@@ -79,9 +97,24 @@ def select(function_code):
         if check_index(item_index):
             destroy(item_index)
 
+    elif function_code == "M":
+        item_index = int(user_input("Item's index number to mark complete: "))
+        if check_index(item_index):
+            if check_complete(item_index) == False:
+                mark_complete(item_index)
+            else:
+                print("{} is already marked complete".format(color_text(checklist[item_index])))
+
+    elif function_code == "N":
+        item_index = int(user_input("Item's index number to mark uncomplete: "))
+        if check_index(item_index):
+            if check_complete(item_index):
+                mark_uncomplete(item_index)
+            else:
+                print("{} is already marked uncomplete".format(color_text(checklist[item_index])))
+
     elif function_code == "P":
         list_all_items()
-        # print(colored('hello', 'red'), colored('world', 'green'))
 
     elif function_code == "Q":
         return False
@@ -97,6 +130,8 @@ def main():
     read_choice_prompt = "[R] Read an item from the list \n"
     update_choice_prompt = "[U] Update an item \n"
     delete_choice_prompt = "[D] Delete an item \n"
+    mark_complete_choice_prompt = "[M] Mark an item as complete \n"
+    mark_uncomplete_choice_prompt = "[N] Mark an item as uncomplete \n"
     print_choice_prompt = "[P] Display all items \n"
     quit_choice_prompt = "[Q] Quit \n"
     prompt_end = "\nYour input: "
@@ -104,13 +139,14 @@ def main():
     user_select = True
     while user_select == True:
         if len(checklist) == 0:
-            selection = user_input(prompt_begin + create_choice_prompt + quit_choice_prompt + prompt_end)
-            print("-----------------------------------")
-            user_select = select(selection.upper())
+            selection = user_input(prompt_begin + create_choice_prompt + quit_choice_prompt + prompt_end).upper()
         else:
-            selection = user_input(prompt_begin + create_choice_prompt + read_choice_prompt + update_choice_prompt + delete_choice_prompt + print_choice_prompt + quit_choice_prompt + prompt_end)
-            print("-----------------------------------")
-            user_select = select(selection.upper())
+            selection = user_input(prompt_begin + create_choice_prompt + read_choice_prompt + update_choice_prompt + delete_choice_prompt + mark_complete_choice_prompt + mark_uncomplete_choice_prompt + print_choice_prompt + quit_choice_prompt + prompt_end).upper()
+        if selection != 'Q':
+            sleep(1)
+            clear()
+        user_select = select(selection.upper())
+    print("Goodbye! Thanks for using the Rainbow Checklist!")
 
 # def test():
     # create("purple sox") # checklist == ["purple sox"]
